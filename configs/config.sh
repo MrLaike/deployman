b4e23ead4b4e03913f1883e1741105f6c64ee5ba
MrLaike <laikelst@gmail.com>
2021-02-19 11:52:02 +0400

    update

--- configs/config.sh
+++ configs/config.sh
@@ -1,13 +1,4 @@
-SSH_USER="vasilida_sopz"
-SSH_PASSWORD="5kipoS&6"
-SSH_HOST="sopz.asap-lp.ru"
-
-# Конфигурация для базы данных
-DB_TYPE="mysql"
-DB_NAME="dbname"
-DB_USER="admin"
-DB_PASSWORD="password"
-DB_PATH_TO_BACKUP="/backup/"
-
-
+SSH_USER=""
+SSH_PASSWORD=""
+SSH_HOST=""
 
